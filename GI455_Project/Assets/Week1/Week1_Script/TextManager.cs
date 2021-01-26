558306e7379d939c98b45ddc182a978358a90fd2
DESKTOP-04OE5IP\Thanop <naporn.lert@gmail.com>
2021-01-26 22:24:15 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Week1/Week1_Script/TextManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Text textBox;
+    public Text answerBox;
+    public Text findText;
+    string[] textSet = { "Mama", "Yamyam", "FF", "Nishin", "Waiwai", "Kanor" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text textBoxAdd;
+        textBoxAdd = textBox.GetComponent<Text>();
+        textBoxAdd.text = textSet[0];
+
+        for (int i = 1; i < textSet.Length; i++)
+        {
+            textBoxAdd.text += "\n"+textSet[i];
+        }
+    }
+
+    public void AnswerReturn()
+    {
+        string inputText = findText.GetComponent<Text>().text;
+        foreach (string t in textSet)
+        {
+            if (inputText == t)
+            {
+                answerBox.GetComponent<Text>().text = "[ <b>" + inputText + "</b> ] is found";
+                return;
+            }
+        }
+        answerBox.GetComponent<Text>().text = "[ <b>" + inputText + "</b> ] is not found";
+    }
+}
